b3486fcfacdfe4d5556f1cc8d8532001842cd3a0
niyas tp <niyastp2000@gmail.com>
2026-04-08 11:03:10 +0530

    api changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://akshaya-backend.vercel.ap/api",
+  baseURL: "https://akshaya-backend.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
